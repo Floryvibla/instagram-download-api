108f82555895b5c5d29ffa0ecebbe676990a662d
Flory <flory.contato@gmail.com>
2026-01-15 00:32:27 -0300

    fix(layout): change default language to pt-br and theme to light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,13 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="pt-br" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
